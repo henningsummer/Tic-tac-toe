48586e0ea5f3c1dcbf6342dbf487239b17ffd6f9
Henning Summer <henningsummer3@gmail.com>
2022-11-22 02:22:57 +0000

    Adjust rules color back option

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ item = "O"
 function clickDiv(t) {
   item == "O" ? "X" : "O";
   
-if (win) {
+if (win || t.innerHTML) {
    return;
 }
 
@@ -24,52 +24,60 @@ if (win) {
   }
 }
 
+msg = document.getElementById("winMsg");
   it = document.querySelectorAll("[class=item]");
 win = false
 
 function getWin(w) {
 
-  console.log(w);
-console.log(it[0].innerHTML);
- 
-if (
- // row
-(it[0].innerHTML == w && 
+if (it[0].innerHTML == w && 
 it[1].innerHTML == w && 
-it[2].innerHTML == w) ||
-(it[3].innerHTML == w && 
+it[2].innerHTML == w){
+  winMsg(w,0,1,2)
+} else if(it[3].innerHTML == w && 
 it[4].innerHTML == w && 
-it[5].innerHTML == w) ||
-(it[6].innerHTML == w && 
+it[5].innerHTML == w){
+  winMsg(w,3,4,5)
+}else if(it[6].innerHTML == w && 
 it[7].innerHTML == w && 
-it[8].innerHTML == w) ||
- // column
-(it[0].innerHTML == w && 
+it[8].innerHTML == w){
+  winMsg(w,6,7,8)
+}else if(it[0].innerHTML == w && 
 it[3].innerHTML == w && 
-it[6].innerHTML == w) ||
-(it[1].innerHTML == w && 
+it[6].innerHTML == w){
+  winMsg(w,0,3,6)
+} else if(it[1].innerHTML == w && 
 it[4].innerHTML == w && 
-it[7].innerHTML == w) ||
-(it[2].innerHTML == w && 
+it[7].innerHTML == w){
+  winMsg(w,1,4,7)
+}else if(it[2].innerHTML == w && 
 it[5].innerHTML == w && 
-it[8].innerHTML == w) ||
- // diagonal
-(it[0].innerHTML == w && 
+it[8].innerHTML == w){
+  winMsg(w,2,5,8)
+}else if(it[0].innerHTML == w && 
 it[4].innerHTML == w && 
-it[8].innerHTML == w) ||
-(it[2].innerHTML == w && 
+it[8].innerHTML == w){
+  winMsg(w,0,4,8)
+}else if(it[2].innerHTML == w && 
 it[4].innerHTML == w && 
-it[6].innerHTML == w)
-) {
-      alert(w +" ganhou!");
-document.getElementById('btn').style.display = "flex";
-
-  win = true
+it[6].innerHTML == w) {
+      winMsg(w,2,4,6)
    } else {
       getDraw()
    }
 }
 
+function winMsg(w,a,b,c) {
+  
+  msg.innerHTML = w +" - ganhou!"
+    msg.style.display = "flex"
+  it[a].style.backgroundColor = "tomato";
+  it[b].style.backgroundColor = "tomato";
+  it[c].style.backgroundColor = "tomato";
+  
+document.getElementById('btn').style.display = "flex";
+  win = true
+}
 
 function getDraw() {
 cont = 0;
@@ -79,7 +87,8 @@ cont = 0;
       }
    }
   if (cont == 9) {
-      alert("empatou!");
+msg.innerHTML ="Empatou!"
+    msg.style.display = "flex"
 document.getElementById('btn').style.display = "flex";
 
   win = true
@@ -89,7 +98,9 @@ document.getElementById('btn').style.display = "flex";
 function newGame() {
    for (let i of it) {
     i.innerHTML = "" ;
+i.style.backgroundColor = "#333"
    }
    win = false
 document.getElementById('btn').style.display = "none"
+  msg.style.display = "none"
 }
